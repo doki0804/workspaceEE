1a6b6d7f8d221f166900b2d9e8017a09630c55d3
21HO <doki0804@naver.com>
2023-01-16 17:22:52 +0900

    ..

--- servletSite/src/main/java/com/itwill/servlet/GugudanTextServlet.java
+++ servletSite/src/main/java/com/itwill/servlet/GugudanTextServlet.java
@@ -12,7 +12,7 @@ public class GugudanTextServlet extends HttpServlet {
 	
 	protected void service( HttpServletRequest request,
 							HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
+		response.setContentType("text/plain;charset=UTF-8");
 		PrintWriter out= response.getWriter();
 		for(int i=1 ; i<=9 ; i++) {
 			for(int j=2 ; j<=9 ; j++) {
