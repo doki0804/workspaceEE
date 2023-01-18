be2b7f3e9b4ea89b71cc8685642754d99b1b3098
21HO <doki0804@naver.com>
2023-01-18 14:54:05 +0900

    ....

--- servletSite/src/main/java/com/itwill/servlet/HttpServletRequestServlet.java
+++ servletSite/src/main/java/com/itwill/servlet/HttpServletRequestServlet.java
@@ -23,16 +23,29 @@ public class HttpServletRequestServlet extends HttpServlet {
 		String remoteAddress = request.getRemoteAddr();
 		
 		/****************************
-		 * << query string >> 
-		 * 	- 클라이언트가 서버로 요청시데이타를 전송하는방법 
-		 * 	- 형식:name1=value1&name2=value2
-		 * ex> name=kim&phone=1234&address=seoul
-		 * 
-		 * 1.GET 요청방식 : request.do?
-		 * << query string >> 
-		 *  ex>request.do?name=kim&phone=1234&address=kyunggi
-		 * 
-		 * 2.POST요청방식 : 책에서 볼께요!!!!
+		<< query string >> 
+  	- 클라이언트가 서버로 요청시데이타를 전송하는방법 
+  	- 형식:name1=value1&name2=value2
+  	- ex> name=kim&phone=1234&address=seoul
+  
+  1.GET  요청방식 : request.do?
+    ex>request.do?name=kim&phone=1234&address=kyunggi
+    			-----------------------------------------------------------------
+    요청라인  	|GET request.do?name=kim&phone=1234&address=kyunggi HTTP/1.1	|	
+    요청헤더  	|HOST:192.168.15.31												|
+    ...			|...															|
+    			|--------------------------------------------------------------	|
+    요청바디	|없다															|
+    (payload)	-----------------------------------------------------------------
+    
+  2.POST 요청방식 : request.do  
+    			-----------------------------------------------------------------
+    요청라인  	|POST request.do HTTP/1.1										|						
+    요청헤더  	|HOST:192.168.15.31												|
+    ...			|...															|
+    			|--------------------------------------------------------------	|
+    요청바디	|name=kim&phone=1234&address=kyunggi							|								|
+     (payload)	-----------------------------------------------------------------
 		 ****************************/
 		//http://localhost/servletSite/request.do?name=kang&phone=909090&address=seoul
 		String queryString=request.getQueryString();
@@ -54,6 +67,11 @@ public class HttpServletRequestServlet extends HttpServlet {
 		out.println("<li>요청URI:"+requestURI+"</li>");
 		out.println("<li>contextPath:"+contextPath+"</li>");
 		out.println("<li>remoteAddress:"+remoteAddress+"</li>");
+		out.println("<li>------------------요청시전송된파라메타------------------</li>");
+		out.println("<li>queryString:"+queryString+"</li>");
+		out.println("<li>name 파라메타값:"+name+"</li>");
+		out.println("<li>phone 파라메타값:"+phone+"</li>");
+		out.println("<li>address 파라메타값:"+address+"</li>");
 		
 		out.println("</ol>");
 		
