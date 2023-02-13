423d4f196d5331bf67565193a9147cb7ca5752b9
21HO <doki0804@naver.com>
2023-02-13 11:42:23 +0900

    ..

--- user_model1/src/main/webapp/sql/userinfo_insert_update_delete_select.sql
+++ user_model1/src/main/webapp/sql/userinfo_insert_update_delete_select.sql
@@ -1,7 +1,7 @@
 
-insert into userinfo(userid,password,name,email) values('guard1','1111','김경호1','guard1@korea.com');
-insert into userinfo(userid,password,name,email) values('guard2','2222','김경호2','guard2@korea.com');
-insert into userinfo(userid,password,name,email) values('guard3','3333','김경호3','guard3@korea.com');
+insert into userinfo(user_id,user_password,user_name,user_phone,user_email,user_address) values('test1','1111','김경호1','guard1@korea.com');
+insert into userinfo(user_id,user_password,user_name,user_phone,user_email,user_address) values('test2','2222','김경호2','guard2@korea.com');
+insert into userinfo(user_id,user_password,user_name,user_phone,user_email,user_address) values('test3','3333','김경호3','guard3@korea.com');
 
 --pk update
 update  userinfo 
